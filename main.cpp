573b809da33a94a51325202dced5b065e1f8a06f
空 新垣 <k023g0045@outlook.jp>
2024-04-17 15:17:55 +0900

    祝！ウィンドウ表示

--- main.cpp
+++ main.cpp
@@ -1,8 +1,80 @@
 #include <Windows.h>
+#include <cstdint>
+#include <string>
+
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	//メッセージに応じて固有の処理を行う
+	switch (msg) {
+
+		//ウィンドウが破壊されたら
+	case WM_DESTROY:
+		//OS対して、アプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+
+
+// windowアプリでのエントリ―ポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-
+	//出力ウィンドウへの文字出力
 	OutputDebugStringA("HEllo,DIrectX!\n");
+	
+
+	WNDCLASS wc{};
+	//ウィンドウプロシージャ
+	wc.lpfnWndProc = WindowProc;
+	//ウィンドウクラス名
+	wc.lpszClassName = L"CG2WindowClass";
+	//インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	//カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+	//ウィンドウクラス登録する
+	RegisterClass(&wc);
+
+	//クライアント領域のサイズ
+	const int32_t kClientWindth = 1280;
+	const int32_t kClientheight = 720;
+
+	RECT wrc = { 0,0,kClientWindth ,kClientheight };
+
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+	//ウィンドウ生成
+	HWND hwad = CreateWindow(
+
+		wc.lpszClassName, L"CG2,",
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT,
+		CW_USEDEFAULT,
+		wrc.right - wrc.left,
+		wrc.bottom - wrc.top,
+		nullptr,
+		nullptr,
+		wc.hInstance,
+		nullptr
+	);
+
+	MSG msg{};
+	//ウィンドウのｘボタンが押されるまでループ
+	ShowWindow(hwad, SW_SHOW);
+
+	while (msg.message != WM_QUIT) {
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+		else {
+
+
+		}
+
+	}
 	return 0;
-
-
+		
 }
