d45c6f7a606f5540ee4f2eda32ab929d0ecf9e16
arakakisora <k023g0045@g.neec.ac.jp>
2024-05-29 16:15:02 +0900

    LoadTexture関数作成

--- main.cpp
+++ main.cpp
@@ -200,6 +200,26 @@ ID3D12DescriptorHeap* CreateDescriptorHeap(
 }
 #pragma endregion 
 
+#pragma region LoadTexture関数
+//LoadTexture
+DirectX::ScratchImage LoadTexture(const std::string& filePath) {
+
+	//テクスチャファイルを読んでプログラムで扱えるようにする
+	DirectX::ScratchImage image{};
+	std::wstring filePathW = ConvertString(filePath);
+	HRESULT hr = DirectX::LoadFromWICFile(filePathW.c_str(), DirectX::WIC_FLAGS_FORCE_SRGB, nullptr, image);
+	assert(SUCCEEDED(hr));
+
+	//ミニマップの作成
+	DirectX::ScratchImage mipImages{};
+	hr = DirectX::GenerateMipMaps(image.GetImages(), image.GetImageCount(), image.GetMetadata(), DirectX::TEX_FILTER_SRGB, 0, mipImages);
+	assert(SUCCEEDED(hr));
+
+	//ミニマップ着きのデータを返す
+	return mipImages;
+}
+#pragma endregion 
+
 // windowアプリでのエントリ―ポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		
