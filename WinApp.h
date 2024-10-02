567b4a95d3fe837adb3bf8c027ad3572b6005088
arakakisora <k023g0045@g.neec.ac.jp>
2024-10-02 15:25:25 +0900

    finalize作成中

--- WinApp.h
+++ WinApp.h
@@ -8,14 +8,29 @@ class WinApp
 
 public:
 
-	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+	//クライアント領域のサイズ
+	static const int32_t kClientWindth = 1280;
+	static const int32_t kClientHeight = 720;
 
 public:
+
+	static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 
 	//初期化
 	void Initialize();
 	//更新
 	void Update();
+
+	void Finalize();
+
+	//Getter
+	HWND GetHwnd()const { return hwnd; }
+	HINSTANCE GetHInstance()const { return wc.hInstance; }
+
+private:
+	//ウィンドウ生成
+	HWND hwnd = nullptr;
+	WNDCLASS wc{};
 
 
 };
