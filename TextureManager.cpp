b9db94067d6712632dd07d7428fd8fadfa6eb92c
arakakisora <k023g0045@g.neec.ac.jp>
2024-11-11 14:46:50 +0900

    debug

--- TextureManager.cpp
+++ TextureManager.cpp
@@ -82,7 +82,7 @@ void TextureManager::LoadTexture(const std::string& filePath)
 	srvDesc.ViewDimension = D3D12_SRV_DIMENSION_TEXTURE2D;//2Dテクスチャ
 	srvDesc.Texture2D.MipLevels = UINT(textureData.metadata.mipLevels);
 
-	dxCommon_->UploadTextureData(textureData.resource, )
+	dxCommon_->UploadTextureData(textureData.resource, mipImages);
 
 	dxCommon_->GetDevice()->CreateShaderResourceView(textureData.resource.Get(), &srvDesc, textureData.srvHandleCPU);
 
