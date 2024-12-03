ee297d20b20e53e02675a0c13a23bab277e3c5a4
arakakisora <k023g0045@g.neec.ac.jp>
2024-12-03 15:11:40 +0900

    確認課題

--- project/main.cpp
+++ project/main.cpp
@@ -37,7 +37,7 @@
 #include "ModelManager.h"
 #include "TextureManager.h"
 #include"ImGuiManager.h"
-
+#include <imgui.h>
 
 
 
@@ -147,115 +147,6 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 
-#pragma region Resource
-	const uint32_t kSubdbivision = 512;
-
-
-	//VertexResourceを作成
-	Microsoft::WRL::ComPtr<ID3D12Resource> vertexResource = dxCommon->CreateBufferResource(sizeof(VertexData) * kSubdbivision * kSubdbivision * 6);
-	//DepthStencilTextureをウィンドウサイズで作成
-
-
-
-
-
-
-
-
-
-	////vertexResource頂点バッファーを作成する
-	D3D12_VERTEX_BUFFER_VIEW vertexBufferView{ };
-	//リソースの先頭のアドレスから使う
-	vertexBufferView.BufferLocation = vertexResource->GetGPUVirtualAddress();
-	//使用するリソースのサイズは頂点分のサイズ
-	vertexBufferView.SizeInBytes = sizeof(VertexData) * kSubdbivision * kSubdbivision * 6;
-	//1頂点当たりのサイズ
-	vertexBufferView.StrideInBytes = sizeof(VertexData);
-	//頂点リソースにデータを書き込む
-	VertexData* vertexData = nullptr;
-	//書き込むためのアドレスを取得
-	vertexResource->Map(0, nullptr, reinterpret_cast<void**>(&vertexData));
-
-	//経度分割1つ分の経度φd
-	const float kLonEvery = 2 * std::numbers::pi_v<float> / (float)kSubdbivision;
-	//緯度分割１つ分の緯度Θd
-	const float kLatEvery = std::numbers::pi_v<float> / (float)kSubdbivision;
-	//緯度方向に分割しながら線を描く
-	const float w = 2.0f;
-	for (uint32_t latIndex = 0; latIndex < kSubdbivision; ++latIndex) {
-		float lat = -std::numbers::pi_v<float> / 2.0f + kLatEvery * latIndex;//θ
-		for (uint32_t lonIndex = 0; lonIndex < kSubdbivision; ++lonIndex) {
-			//テクスチャ用のTexcood
-
-			//書き込む最初の場所
-			uint32_t start = (latIndex * kSubdbivision + lonIndex) * 6;
-			float lon = lonIndex * kLonEvery;//∮
-			//基準点a
-			vertexData[start].position.x = std::cosf(lat) * std::cosf(lon);
-			vertexData[start].position.y = std::sinf(lat);
-			vertexData[start].position.z = std::cosf(lat) * std::sinf(lon);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex) / float(kSubdbivision), 1.0f - float(latIndex) / float(kSubdbivision) };
-			//基準点b
-			start++;
-			vertexData[start].position.x = std::cosf(lat + kLatEvery) * std::cosf(lon);
-			vertexData[start].position.y = std::sinf(lat + kLatEvery);
-			vertexData[start].position.z = std::cosf(lat + kLatEvery) * std::sinf(lon);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex) / float(kSubdbivision), 1.0f - float(latIndex + 1.0f) / float(kSubdbivision) };
-			//基準点c
-			start++;
-			vertexData[start].position.x = std::cosf(lat) * std::cosf(lon + kLonEvery);
-			vertexData[start].position.y = std::sinf(lat);
-			vertexData[start].position.z = std::cosf(lat) * std::sinf(lon + kLonEvery);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex + 1.0f) / float(kSubdbivision), 1.0f - float(latIndex) / float(kSubdbivision) };
-
-			//基準点c
-			start++;
-			vertexData[start].position.x = std::cosf(lat) * std::cosf(lon + kLonEvery);
-			vertexData[start].position.y = std::sinf(lat);
-			vertexData[start].position.z = std::cosf(lat) * std::sinf(lon + kLonEvery);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex + 1.0f) / float(kSubdbivision), 1.0f - float(latIndex) / float(kSubdbivision) };
-
-			//基準点b
-			start++;
-			vertexData[start].position.x = std::cosf(lat + kLatEvery) * std::cosf(lon);
-			vertexData[start].position.y = std::sinf(lat + kLatEvery);
-			vertexData[start].position.z = std::cosf(lat + kLatEvery) * std::sinf(lon);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex) / float(kSubdbivision), 1.0f - float(latIndex + 1.0f) / float(kSubdbivision) };
-
-			//基準点d
-			start++;
-			vertexData[start].position.x = std::cosf(lat + kLatEvery) * std::cosf(lon + kLonEvery);
-			vertexData[start].position.y = std::sinf(lat + kLatEvery);
-			vertexData[start].position.z = std::cosf(lat + kLatEvery) * std::sinf(lon + kLonEvery);
-			vertexData[start].position.w = w;
-			vertexData[start].normal.x = vertexData[start].position.x;
-			vertexData[start].normal.y = vertexData[start].position.y;
-			vertexData[start].normal.z = vertexData[start].position.z;
-			vertexData[start].texcoord = { float(lonIndex + 1) / float(kSubdbivision), 1.0f - float(latIndex + 1) / float(kSubdbivision) };
-		}
-
-
-	}
 
 
 
@@ -271,25 +162,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 
-	//マテリアる用のリソースを作る。今回color1つ分のサイズを用意する
-	Microsoft::WRL::ComPtr<ID3D12Resource> materialResource = dxCommon->CreateBufferResource(sizeof(Material));
-	//マテリアルにデータを書き込む	
-	Material* materialDataSphere = nullptr;
-	materialResource->Map(0, nullptr, reinterpret_cast<void**>(&materialDataSphere));
-	//色
-	materialDataSphere->color = { Vector4(1.0f, 1.0f, 1.0f, 1.0f) };
-	materialDataSphere->enableLighting = true;//有効にするか否か
-	materialDataSphere->uvTransform = MekeIdentity4x4();
 
-	//WVP用のリソースを作る。Matrix4x4 1つ分のサイズを用意する
-	Microsoft::WRL::ComPtr<ID3D12Resource> wvpResource = dxCommon->CreateBufferResource(sizeof(TransformationMatrix));
-	//データを書き込む
-	TransformationMatrix* wvpData = nullptr;
-	//書き込むためのアドレスを取得
-	wvpResource->Map(0, nullptr, reinterpret_cast<void**>(&wvpData));
-	//単位行列を書き込む
-	wvpData->WVP = MakeIdentity4x4();
-	wvpData->World = MakeIdentity4x4();
+
 
 
 
@@ -386,20 +260,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		imGuiMnager->Begin();
 		input->Update();
 
-		////更新
-		//if (input->TriggerKey(DIK_0)) {
 
-		//	transform.rotate.y += 0.3f;
-
-		//}
-
-		/*Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(WinApp::kClientWindth) / float(WinApp::kClientHeight), 0.1f, 100.0f);
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		wvpData->WVP = worldViewProjectionMatrix;
-		wvpData->World = worldMatrix;*/
 
 
 		for (Sprite* sprite : sprites) {
@@ -418,81 +279,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		object3D2nd->SetRotate(Vector3{ 0,0 ,rotation });
 		object3D2nd->Update();
 
-		/*	Matrix4x4 uvTransformMatrix = MakeScaleMatrix(uvTransformSprite.scale);
-			uvTransformMatrix = Multiply(uvTransformMatrix, MakeRotateZMatrix(uvTransformSprite.rotate.z));
-			uvTransformMatrix = Multiply(uvTransformMatrix, MakeTranslateMatrix(uvTransformSprite.translate));
-			materialDataSprite->uvTransform = uvTransformMatrix;*/
 
 
 
-			////CameraTransform
-			//if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen))
-			//{/*
-			//	ImGui::DragFloat3("CameraTranslate", &cameraTransform.translate.x, 0.01f);
-			//	ImGui::DragFloat3("CameraRotate", &cameraTransform.rotate.x, 0.01f);*/
-			//}
-			//ImGui::Checkbox("useMonsterBall", &useMonsterBall);
-			//// SphereSetColor
-			//if (ImGui::CollapsingHeader("SetcolorSphere", ImGuiTreeNodeFlags_DefaultOpen))
-			//{
-			//	ImGui::ColorEdit4("*SetColor", &materialDataSphere->color.x);
-			//}
-			//// SphereTransform
-			//if (ImGui::CollapsingHeader("Sphere", ImGuiTreeNodeFlags_DefaultOpen))
-			//{
-			//	ImGui::DragFloat3("*Scale", &transform.scale.x, 0.01f);
-			//	ImGui::DragFloat3("*Rotate", &transform.rotate.x, 0.01f);
-			//	ImGui::DragFloat3("*Transrate", &transform.translate.x, 0.01f);
-			//}
-			//// ModelTransform
-			//if (ImGui::CollapsingHeader("Model", ImGuiTreeNodeFlags_DefaultOpen))
-			//{
-			//	transformModel = object3D->GetTransform();
 
-			//	ImGui::DragFloat3("*ModelScale", &transformModel.scale.x, 0.01f);
-			//	ImGui::DragFloat3("*ModelRotate", &transformModel.rotate.x, 0.01f);
-			//	ImGui::DragFloat3("*ModelTransrate", &transformModel.translate.x, 0.01f);
+		//CameraTransform
+		if (ImGui::CollapsingHeader("Camera", ImGuiTreeNodeFlags_DefaultOpen))
+		{/*
+			ImGui::DragFloat3("CameraTranslate", &cameraTransform.translate.x, 0.01f);
+			ImGui::DragFloat3("CameraRotate", &cameraTransform.rotate.x, 0.01f);*/
+		}
+		ImGui::Checkbox("useMonsterBall", &useMonsterBall);
+			// ModelTransform
+		if (ImGui::CollapsingHeader("Model", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			transformModel = object3D->GetTransform();
 
-			//	object3D->SetTransform(transformModel);
-			//}
-			////if (ImGui::CollapsingHeader("Sprite", ImGuiTreeNodeFlags_DefaultOpen))
-			////{
-			////	//SpriteTransform
-			////	Vector2 size = sprite->GetSize();
-			////	Vector2 position = sprite->GetPosition();
-			////	float rotation = sprite->GetRotation();
-			////	Vector4 spritecolor = sprite->GetColor();
+			ImGui::DragFloat3("*ModelScale", &transformModel.scale.x, 0.01f);
+			ImGui::DragFloat3("*ModelRotate", &transformModel.rotate.x, 0.01f);
+			ImGui::DragFloat3("*ModelTransrate", &transformModel.translate.x, 0.01f);
 
-			////	ImGui::ColorEdit4("*spriteColor", &spritecolor.x);
-			////	ImGui::DragFloat2("*ScaleSprite", &size.x, 0.1f);
-			////	ImGui::DragFloat("*RotateSprite", &rotation, 0.1f);
-			////	ImGui::DragFloat2("*TransrateSprite", &position.x);
-			////	sprite->setColor(spritecolor);
-			////	sprite->SetPosition(position);
-			////	sprite->SetRotation(rotation);
-			////	sprite->SetSize(size);
-			////}
-			////uvTransformSprite
-			//if (ImGui::CollapsingHeader("uvTransformSprite", ImGuiTreeNodeFlags_DefaultOpen))
-			//{
+			object3D->SetTransform(transformModel);
+		}
+		
 
-			//	/*ImGui::DragFloat2("*UVTranslate", &uvTransformSprite.translate.x, 0.01f, -10.0f, 10.0f);
-			//	ImGui::DragFloat2("*UVScale", &uvTransformSprite.scale.x, 0.01f, -1.0f, 1.0f);
-			//	ImGui::SliderAngle("*UVRotate", &uvTransformSprite.rotate.z);*/
-
-			//}
-
-			////項目4
-			//if (ImGui::CollapsingHeader("directionalLight", ImGuiTreeNodeFlags_DefaultOpen))
-			//{
-			//	/*ImGui::ColorEdit4("*LightSetColor", &directionalLightData->color.x);
-			//	ImGui::DragFloat3("*Lightdirection", &directionalLightData->direction.x, 0.01f, -1.0f, 1.0f);*/
-			//}
+		//項目4
+		if (ImGui::CollapsingHeader("directionalLight", ImGuiTreeNodeFlags_DefaultOpen))
+		{
+			/*ImGui::ColorEdit4("*LightSetColor", &directionalLightData->color.x);
+			ImGui::DragFloat3("*Lightdirection", &directionalLightData->direction.x, 0.01f, -1.0f, 1.0f);*/
+		}
 
 
 
 		imGuiMnager->End();
-
 		//DirectXの描画準備。すべての描画に共通のグラフィックスコマンドを積む
 		dxCommon->Begin();
 
@@ -515,60 +335,29 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 			sprite->Draw();
 
-
 		}
 
 #pragma endregion
 
 
-
-
-		////Sphere
-		//dxCommon->GetCommandList()->IASetVertexBuffers(0, 1, &vertexBufferView);
-		////現状を設定。POSに設定しているものとはまた別。おなじ物を設定すると考えておけばいい
-		//dxCommon->GetCommandList()->SetGraphicsRootConstantBufferView(0, materialResource->GetGPUVirtualAddress());
-		////wvp用のCBufferの場所を設定
-		//dxCommon->GetCommandList()->SetGraphicsRootConstantBufferView(1, wvpResource->GetGPUVirtualAddress());
-		//dxCommon->GetCommandList()->SetGraphicsRootDescriptorTable(2, textureSrvHandleGPU);
-		//dxCommon->GetCommandList()->SetGraphicsRootConstantBufferView(3, directionalLightResource->GetGPUVirtualAddress());
-		////描画！
-		////dxCommon->GetCommandList()->DrawInstanced(kSubdbivision * kSubdbivision * 6, 1, 0, 0);
-
-
-
-
-
-		//ImGui_ImplDX12_RenderDrawData(ImGui::GetDrawData(), dxCommon->GetCommandList());
-
-
-
+		//imgui描画処理
 		imGuiMnager->Draw();
-
+		//dx共通部PostDraw
 		dxCommon->End();
 
-
 	}
-
-
-#pragma region Release
-
-
-
-
+#pragma region Finalize処理
+	//imgui終了処理
 	imGuiMnager->Finalize();
-
-
-#ifdef _DEBUG
-
-#endif // _DEBUG
-#pragma endregion
-
-
 	//WindowsAPI終了処理
 	winApp->Finalize();
 	//WindowsAPI解放
 	TextureManager::GetInstance()->Finalize();
 	ModelManager::GetInstans()->Finalize();
+
+#pragma endregion
+
+#pragma region Release
 	delete winApp;
 	delete dxCommon;
 	delete imGuiMnager;
@@ -584,7 +373,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	delete object3D;
 	delete object3D2nd;
 
-
+#pragma endregion
 
 	return 0;
 
