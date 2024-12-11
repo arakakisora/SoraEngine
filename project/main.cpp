273d6cfb209956895bc455e1999e4a56fb531ec8
arakakisora <k023g0045@g.neec.ac.jp>
2024-12-11 10:21:37 +0900

    不要コードを削除

--- project/main.cpp
+++ project/main.cpp
@@ -127,14 +127,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	//テクスチャの初期化
 	std::string textureFilePath[2]{ "Resources/monsterBall.png" ,"Resources/uvChecker.png" };
 
-	//スプライトの初期化
-	std::vector<Sprite*>sprites;
-	for (uint32_t i = 0; i < 12; ++i) {
-		Sprite* sprite = new Sprite();
-		sprite->Initialize(spriteCommon, textureFilePath[1]);
-		sprites.push_back(sprite);
-
-	}
+	
 	ModelManager::GetInstans()->LoadModel("plane.obj");
 	ModelManager::GetInstans()->LoadModel("axis.obj");
 
@@ -159,28 +152,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 
 	int i = 0;
-	for (Sprite* sprite : sprites) {
-		Vector2 position = sprite->GetPosition();
-		Vector2 size = sprite->GetSize();
+	
 
-		position.x = 100.0f * i;
-		position.y = 200.0f;
-		size = Vector2(100, 100);
-
-
-
-		sprite->SetPosition(position);
-		sprite->SetSize(size);
-		sprite->SetAnchorPoint(Vector2{ 0.0f,0.0f });
-		sprite->SetIsFlipY(0);
-		sprite->SetTextureLeftTop(Vector2{ i * 64.0f,0.0f });
-		sprite->SetTextureSize(Vector2{ 64.0f,64.0f });
-
-		i++;
-
-	}
-
-	float rotation{ 0 };
 
 
 	//wvpData用のTransform変数を作る
@@ -224,36 +197,16 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		
 
 
-		////更新
-		//if (input->TriggerKey(DIK_0)) {
-
-		//	transform.rotate.y += 0.3f;
-
-		//}
-
-		/*Matrix4x4 worldMatrix = MakeAffineMatrix(transform.scale, transform.rotate, transform.translate);
-		Matrix4x4 cameraMatrix = MakeAffineMatrix(cameraTransform.scale, cameraTransform.rotate, cameraTransform.translate);
-		Matrix4x4 viewMatrix = Inverse(cameraMatrix);
-		Matrix4x4 projectionMatrix = MakePerspectiveFovMatrix(0.45f, float(WinApp::kClientWindth) / float(WinApp::kClientHeight), 0.1f, 100.0f);
-		Matrix4x4 worldViewProjectionMatrix = Multiply(worldMatrix, Multiply(viewMatrix, projectionMatrix));
-		wvpData->WVP = worldViewProjectionMatrix;
-		wvpData->World = worldMatrix;*/
+		
 
 
-		for (Sprite* sprite : sprites) {
+		
 
-			//rotation.x+=0.03f;
-			//sprite->SetRotation(rotation.x);
-			sprite->Update();
-
-
-		}
-
-		rotation += 0.03f;
-		object3D->SetRotate(Vector3{ 0,rotation ,0 });
+		
+		
 		object3D->Update();
 
-		object3D2nd->SetRotate(Vector3{ 0,0 ,rotation });
+		
 		object3D2nd->Update();
 
 #ifdef _DEBUG
@@ -296,12 +249,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		spriteCommon->CommonDraw();
 		/*sprite->Draw();*/
 
-		for (Sprite* sprite : sprites) {
-
-			sprite->Draw();
-
-
-		}
+		
 
 #pragma endregion
 
@@ -340,9 +288,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	//スプライト解放
 	delete spriteCommon;
-	for (Sprite* sprite : sprites) {
-		delete sprite; // 各Spriteオブジェクトの削除
-	}
+	
 	//3Dオブジェクト解放
 	delete object3DCommon;
 	delete object3D;
