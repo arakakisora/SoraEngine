a3a45db05eac51bfa64a9c38de2222d0e71aec48
arakakisora <k023g0045@g.neec.ac.jp>
2025-01-23 15:01:59 +0900

    レベルアップ

--- project/engine/3d/Model.h
+++ project/engine/3d/Model.h
@@ -13,6 +13,11 @@ public:
 	/// 描画
 	/// </summary>
 	void Draw();
+	//void ParticleDraw(uint32_t instanceCount);
+
+	D3D12_VERTEX_BUFFER_VIEW GetVertexBufferView()const { return vertexBufferView; }
+	ModelData GetModelData() { return modelData; }
+
 	
 
 	static MaterialData LoadMaterialTemplateFile(const std::string& directorypath, const std::string& filename);
